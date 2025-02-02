1182126a3f5e3cc33c84ea347dd76f5ce2a134fa
Barend Scholtus <barend.scholtus@gmail.com>
2025-02-02 18:17:35 +0700

    Enable Vial security. WPM settings. New font file. OLED settings. Tapping settings.

--- keyboards/neodox_ergomech/keymaps/vial/config.h
+++ keyboards/neodox_ergomech/keymaps/vial/config.h
@@ -20,9 +20,33 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Use I2C or Serial, not both */
 #define USE_SERIAL
 // #define USE_I2C
+
 /* Select hand configuration */
 #define MASTER_LEFT
-//#define MASTER_RIGHT
+// #define MASTER_RIGHT
 // #define EE_HANDS
-#define VIAL_KEYBOARD_UID {0x54, 0xB9, 0x18, 0x3D, 0xE9, 0x29, 0xA8, 0x22}
+
+#define VIAL_KEYBOARD_UID { 0x54, 0xB9, 0x18, 0x3D, 0xE9, 0x29, 0xA8, 0x22 }
 #define DYNAMIC_KEYMAP_LAYER_COUNT 10
+
+// https://get.vial.today/docs/porting-to-vial.html
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 4 } // top left and bottom left corner
+#define VIAL_UNLOCK_COMBO_COLS { 0, 0 }
+
+// tapping settings don't work on the Vial UI
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define QUICK_TAP_TERM 100
+#define HOLD_ON_OTHER_KEY_PRESS
+
+// for the displays. oled_timeout is needed for Ocean dream to prevent screen off/on bug
+#define OLED_FADE_OUT
+#define OLED_FADE_OUT_INTERVAL 10
+#define OLED_TIMEOUT 60000
+
+// https://docs.qmk.fm/features/oled_driver#logo-example
+#define OLED_FONT_H "font.c"
+
+// https://docs.qmk.fm/features/wpm
+// #define WPM_SAMPLE_SECONDS 10
+#define WPM_LAUNCH_CONTROL 1
--- keyboards/neodox_ergomech/keymaps/vial/rules.mk
+++ keyboards/neodox_ergomech/keymaps/vial/rules.mk
@@ -1,9 +1,22 @@
-VIA_ENABLE=yes
-VIAL_ENABLE=yes
-LTO_ENABLE=yes
-OLED_ENABLE=yes
-CONSOLE_ENABLE=yes
-EXTRAKEY_ENABLE=yes
-ENCODER_ENABLE=yes
-ENCODER_MAP_ENABLE=yes
-VIAL_INSECURE=yes
+VIA_ENABLE = yes
+VIAL_ENABLE = yes
+LTO_ENABLE = yes
+OLED_ENABLE = yes
+CONSOLE_ENABLE = no
+EXTRAKEY_ENABLE = yes
+ENCODER_ENABLE = yes
+ENCODER_MAP_ENABLE = yes
+WPM_ENABLE = yes
+OS_DETECTION_ENABLE = no
+OCEAN_DREAM_ENABLE = yes
+PERMISSIVE_HOLD = yes  # does not work on Vial UI
+
+# https://github.com/snowe2010/qmk_firmware/blob/ocean-dream/users/snowe/readme_ocean_dream.md
+ifeq ($(strip $(OLED_ENABLE)), yes)
+    ifdef OCEAN_DREAM_ENABLE
+        ifeq ($(strip $(OCEAN_DREAM_ENABLE)), yes)
+            SRC += ocean_dream.c
+            OPT_DEFS += -DOCEAN_DREAM_ENABLE
+        endif
+    endif
+endif
